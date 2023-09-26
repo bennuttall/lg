89faee22fdbdbf43e309e1e1def4de303d27faae
Ben Nuttall <ben@bennuttall.com>
2023-09-26 10:43:20 +0100

    Add previous directory to extra compile args

--- PY_LGPIO/setup.py
+++ PY_LGPIO/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, Extension
 with open('README.md') as f:
     long_description = f.read()
 
-lgpio_module = Extension('_lgpio', sources=['lgpio_wrap.c',], libraries=['lgpio',],)
+lgpio_module = Extension('_lgpio', sources=['lgpio_wrap.c',], libraries=['lgpio',], extra_compile_args=['-I../'])
 
 setup (name = 'lgpio',
        version = '0.2.2.0',
